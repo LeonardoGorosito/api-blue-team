9588ac5a594d0800e294a8e5d479f594c5988a94
LeoG <leogorosito.lg@gmail.com>
2025-11-21 04:18:00 -0300

    clean .jss

--- src/server.ts
+++ src/server.ts
@@ -93,15 +93,20 @@ await app.register(payments, { prefix: '/payments' })
 // Código para arrancar en local (npm run dev)
 if (process.env.NODE_ENV !== 'production') {
   const start = async () => {
-    try {
-      const port = Number(ENV.PORT) || 3000;
-      await app.listen({ port: port, host: '0.0.0.0' });
-      app.log.info(`Servidor HTTP corriendo en http://localhost:${port}`);
-    } catch (err) {
-      app.log.error(err);
-      process.exit(1);
-    }
-  };
+  try {
+    const port = Number(ENV.PORT) || 3000
+    await app.listen({ port, host: '0.0.0.0' })
+    app.log.info(`Servidor HTTP corriendo en http://localhost:${port}`)
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+
+// Si querés evitar que arranque solo en tests, podés dejar esta pequeña condición:
+if (process.env.NODE_ENV !== 'test') {
+  start()
+};
   start();
 }
 
